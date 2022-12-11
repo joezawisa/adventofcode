898ee58ea425209d06e381d4894227d01d0b0a26
Joe Zawisa <36611576+joezawisa@users.noreply.github.com>
2022-12-11 00:45:52 -0500

    Day three, part one

---
+++ 2022/03/part1.py
@@ -0,0 +1,33 @@
+# https://adventofcode.com/2022/day/3
+
+import sys
+
+# Check for proper invocation
+if len(sys.argv) != 2:
+
+    print(f'Usage: {sys.argv[0]} <filename>', file=sys.stderr)
+    sys.exit(1)
+
+# Read input file
+with open(sys.argv[1], 'r') as file:
+
+    # Keep a running total of the priorities of all the items that are packed incorrectly
+    total = 0
+
+    # Check each backpack
+    for line in file:
+
+        line = line.strip()
+
+        # Grab each item from the first compartment
+        for item in line[:len(line) // 2]:
+
+            # And check whether it's in the second compartment
+            if item in line[len(line) // 2:]:
+
+                # When we find the item in both compartments, add its priority to the total
+                total += ord(item) - 96 if item.islower() else ord(item) - 38
+                break
+
+# Present the result!
+print(total, file=sys.stdout)
